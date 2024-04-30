17fe2358ab62cd5c44a1cddb52e5ad6d8e730fcc
Sintiria <Jaydentomlinson4@gmail.com>
2024-04-30 18:12:30 -0400

    outline for user auth class

---
+++ website/auth.py
@@ -0,0 +1,14 @@
+import re
+
+class UserAuth:
+   ...
+   
+"""
+Funcitons
+-potentially create an auth class for these-
+
+- username duplicate avoidance
+- email re
+- password strength check
+     
+"""
